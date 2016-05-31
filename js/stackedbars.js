2e90b9449780f8a8063bfe55f82012e001f4062f
Lex Favrin <a.s.favrin@student.utwente.nl>
2016-05-31 09:43:29 +0200

    Changed color

--- js/stackedbars.js
+++ js/stackedbars.js
@@ -28,7 +28,7 @@ function CreateStackedBarChart(dataNest, selector) {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   var color = d3.scale.ordinal()
-      .range(["#ff0000", "#00ff00", "#0000ff"]);
+      .range(["#f70000", "#ff2626", "#ff5353"]);
 
   var mappedData = [];
   var totalpopulation = 0, totalliteracy = 0, totalemployment = 0;
